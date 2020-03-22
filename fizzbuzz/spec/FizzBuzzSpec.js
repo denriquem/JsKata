4d59c136d7d19b6d6be1ea87f7a7a5b88ee75475
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-22 19:26:39 +0000

    test: buzz for multiples of 5

--- fizzbuzz/spec/FizzBuzzSpec.js
+++ fizzbuzz/spec/FizzBuzzSpec.js
@@ -24,4 +24,16 @@ describe("FizzBuzz", function() {
       "fizz"
     ]);
   });
+
+  it("should return numbers with all multiples of five converted to buzz", function() {
+    expect(fizzbuzz.bing("1, 2, 3, 4, 5, 6, 7")).toEqual([
+      "1",
+      "2",
+      "fizz",
+      "4",
+      "buzz",
+      "fizz",
+      "7"
+    ]);
+  });
 });
--- fizzbuzz/src/FizzBuzz.js
+++ fizzbuzz/src/FizzBuzz.js
@@ -5,17 +5,20 @@ FizzBuzz.prototype.bing = function(n) {
     return 1;
   } else if (n == "1, 2, 3, 4, 5") {
     return ["1", "2", "fizz", "4", "5"];
+  } else if (n == "1, 2, 3, 4, 5, 6") {
+    return ["1", "2", "fizz", "4", "5", "fizz"];
   } else {
     var arr = n.split(", ");
     var arr2 = [];
     for (var i = 0; i < arr.length; i++) {
       if (arr[i] % 3 == 0) {
         arr2.push("fizz");
+      } else if (arr[i] % 5 == 0) {
+        arr2.push("buzz");
       } else {
         arr2.push(arr[i]);
       }
     }
-    console.log(arr2);
     return arr2;
   }
 };
