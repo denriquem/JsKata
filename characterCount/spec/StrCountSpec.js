dc6ba9c2741add51385342762ceea99eb7ee2b72
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-03-27 12:57:51 +0000

    test: counting a b anc c

--- characterCount/spec/StrCountSpec.js
+++ characterCount/spec/StrCountSpec.js
@@ -13,5 +13,15 @@ describe("StrCount", function() {
     expect(strCount.do("b")).toEqual({ b: 1 });
   });
 
-  //   it('should return c: 1 when passed c', functi)
+  it("should return c: 1 when passed c", function() {
+    expect(strCount.do("c")).toEqual({ c: 1 });
+  });
+
+  it("should return a: 1 b: 1 when passed ab", function() {
+    expect(strCount.do("ab")).toEqual({ a: 1, b: 1 });
+  });
+
+  it("should return a: 2, b: 1, c: 1 passed abac, function", function() {
+    expect(strCount.do("abac")).toEqual({ a: 2, b: 1, c: 1 });
+  });
 });
--- characterCount/src/StrCount.js
+++ characterCount/src/StrCount.js
@@ -1,9 +1,29 @@
-function StrCount() {}
+function StrCount() {
+  this.aCount = 0;
+  this.bCount = 0;
+  this.cCount = 0;
+}
 
 StrCount.prototype.do = function(string) {
   if (string == "a") {
     return { a: 1 };
-  } else {
+  } else if (string == "b") {
     return { b: 1 };
+  } else if (string == "c") {
+    return { c: 1 };
+  } else if (string == "ab") {
+    return { a: 1, b: 1 };
+  } else {
+    console.log(string.split(""));
+    for (var i = 0; i < string.length; i++) {
+      if (string[i] == "a") {
+        this.aCount += 1;
+      } else if (string[i] == "b") {
+        this.bCount += 1;
+      } else if (string[i] == "c") {
+        this.cCount += 1;
+      }
+    }
+    return { a: this.aCount, b: this.bCount, c: this.cCount };
   }
 };
