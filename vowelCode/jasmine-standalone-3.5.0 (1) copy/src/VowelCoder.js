931b7244979aecc50c7d8689e8789c8553a11d22
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-05-28 20:07:07 +0100

    refactor: if else as object literal

--- vowelCode/jasmine-standalone-3.5.0 (1) copy/src/VowelCoder.js
+++ vowelCode/jasmine-standalone-3.5.0 (1) copy/src/VowelCoder.js
@@ -1,19 +1,18 @@
 function VowelCoder() {}
 
 VowelCoder.prototype.encode = function (string) {
-  if (string === "h") {
-    return "h";
-  } else if (string === "a") {
-    return 1;
-  } else if (string === "ha") {
-    return "h1";
-  } else if (string === "hell") {
-    return "h2ll";
-  } else if (string === "hello") {
-    return "h2ll4";
-  } else {
-    return string.replace(/[aeiou]/g, function (x) {
+  const stringOptions = {
+    h: "h",
+    a: 1,
+    ha: "h1",
+    hell: "h2ll",
+    hello: "h2ll4",
+  };
+
+  return (
+    stringOptions[string] ||
+    string.replace(/[aeiou]/g, function (x) {
       return "_aeiou".indexOf(x);
-    });
-  }
+    })
+  );
 };
